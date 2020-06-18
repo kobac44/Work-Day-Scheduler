545682efb43095741bdce2e76f978d67a2088a79
Unko <kobie.watkins@gmail.com>
2020-06-18 10:08:36 -0400

    ran into a wall understanding local storage and how to get it to function as well as the scheduler itself, today able to complete a few new train of thoughts for functions

--- script.js
+++ script.js
@@ -19,4 +19,26 @@ $currentDay.text(now);
 //localStorage.content = $("#test").html("Test");
 //$("#test").html(localStorage.content);
 
-// saves data to localStorage
+$(document).ready(function () {
+  let timeEl = $("<p>");
+  let dayEl = $("<p>");
+  let data1 = $("input1");
+  let data2 = $("input2");
+  let data3 = $("input3");
+  let data4 = $("input4");
+  let data5 = $("input5");
+  let data12 = $("input12");
+  let data11 = $("input11");
+  let data10 = $("input10");
+  let data9 = $("input9");
+
+  data1.val(localStorage.getItem("Entry1"));
+  data2.val(localStorage.getItem("Entry2"));
+  data3.val(localStorage.getItem("Entry3"));
+  data4.val(localStorage.getItem("Entry4"));
+  data5.val(localStorage.getItem("Entry5"));
+  data9.val(localStorage.getItem("Entry9"));
+  data10.val(localStorage.getItem("Entry10"));
+  data11.val(localStorage.getItem("Entry11"));
+  data12.val(localStorage.getItem("Entry12"));
+});
