5a8adea6ddead238c1e24a5aab2dbd1e58a9c5dd
Unko <kobie.watkins@gmail.com>
2020-06-19 06:18:14 -0400

    my entire first js code fail and now working wwith a new one, hoping it works... easier than the firstand less complicated

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //show time using Momentjs format
-//$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
+$("#currentDay").text(
+  moment().format("dddd") + "," + moment().format("MMMM, Do, YYYY, h:mm:ss a")
+);
 // const $currentDay = $("#currentDay");
 // const $container = $(".container");
 // let now = moment().format("dddd, MMMM Do");
@@ -19,60 +21,51 @@
 //localStorage.content = $("#test").html("Test");
 //$("#test").html(localStorage.content);
 
-$(document).ready(function () {
-  let timeEl = $("<p>");
-  let dayEl = $("<p>");
-  let data1 = $("input1");
-  let data2 = $("input2");
-  let data3 = $("input3");
-  let data4 = $("input4");
-  let data5 = $("input5");
-  let data12 = $("input12");
-  let data11 = $("input11");
-  let data10 = $("input10");
-  let data9 = $("input9");
+//Assign saveBtn click listener for user input and time stamp??
+$(".saveBtn").on("click", function () {
+  //get nearby values.
+  console.log(this);
+  var text = $(this).siblings(".list").val();
+  var time = $(this).parent().attr("id");
 
-  data1.val(localStorage.getItem("Entry1"));
-  data2.val(localStorage.getItem("Entry2"));
-  data3.val(localStorage.getItem("Entry3"));
-  data4.val(localStorage.getItem("Entry4"));
-  data5.val(localStorage.getItem("Entry5"));
-  data9.val(localStorage.getItem("Entry9"));
-  data10.val(localStorage.getItem("Entry10"));
-  data11.val(localStorage.getItem("Entry11"));
-  data12.val(localStorage.getItem("Entry12"));
-
-  let button9 = $("#btn9");
-  let button10 = $("#btn10");
-  let button11 = $("#btn11");
-  let button12 = $("#btn12");
-  let button1 = $("#btn1");
-  let button2 = $("#btn2");
-  let button3 = $("#btn3");
-  let button4 = $("#btn4");
-  let button5 = $("#btn5");
-
-  const timeNow = parseInt(moment().format("HH"));
-
-  function clear() {
-    localStorage.clear();
-    data1.val("");
-    data2.val("");
-    data3.val("");
-    data4.val("");
-    data5.val("");
-    data9.val("");
-    data10.val("");
-    data11.val("");
-    data12.val("");
-  }
-
-  function clock() {
-    dayEl.text(momet().format("LL"));
-    timeEl.text(moment().format("hh:mm:ssA"));
-    $(".jumbotron").append(dayEl);
-    $(".jumbotron").apprend(timeEl);
-  }
-
-  function checkTime() {}
+  //set items in local storage.
+  localStorage.setItem(time, text);
 });
+
+//load any saved data from LocalStorage - do this for each hour created.
+$("9AM .list").val(localStorage.getItem("hour9"));
+$("#10AM .list").val(localStorage.getItem("hour10"));
+$("#11AM .list").val(localStorage.getItem("hour11"));
+$("#12PM .list").val(localStorage.getItem("hour12"));
+$("#1 .list").val(localStorage.getItem("hour1"));
+$("#2 .list").val(localStorage.getItem("hour2"));
+$("#3 .list").val(localStorage.getItem("hour3"));
+$("#4 .list").val(localStorage.getItem("hour4"));
+$("#5 .list").val(localStorage.getItem("hour5"));
+
+//get current number of hours.
+var currentHour = moment().hour();
+
+Function(currentHour);
+
+function newFunction(currentHour) {
+  $(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+    console.log(blockHour, currenthour);
+  });
+}
+
+//check if we've moved past this time
+if ("blockHour" < currentHour) {
+  $(this).addClass("past");
+  $(this).removeClass("future");
+  $(this).removeClass("present");
+} else if ("blockHour" === currentHour) {
+  $(this).removeClass("past");
+  $(this).addClass("present");
+  $(this).removeClass("future");
+} else {
+  $(this).removeClass("present");
+  $(this).removeClass("past");
+  $(this).addClass("future");
+}
