bca90d18a4d79ddb2244e81be8f48986be9f9e94
Unko <kobie.watkins@gmail.com>
2020-06-19 12:59:46 -0400

    edit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //show time using Momentjs format
-$("#currentDay").text(
-  moment().format("dddd") + "," + moment().format("MMMM, Do, YYYY, h:mm:ss a")
-);
+// $("#currentDay").text(
+//   moment().format("dddd") + "," + moment().format("MMMM, Do, YYYY, h:mm:ss a")
+// );
 // const $currentDay = $("#currentDay");
 // const $container = $(".container");
 // let now = moment().format("dddd, MMMM Do");
